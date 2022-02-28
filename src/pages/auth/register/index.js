68ef669a2937e61152aad0ac6a0019b7ce607231
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-28 15:31:07 +0630

    register page added

--- src/pages/auth/register/index.js
+++ src/pages/auth/register/index.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import AuthBox from "../../../shared/components/AuthBox";
+import { validateRegisterForm } from "../../../shared/utils/validators";
+import RegisterPageFooter from "./footer";
+import RegisterForm from "./RegisterForm";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    setIsFormValid(validateRegisterForm({ mail, username, password }));
+  }, [mail, password, username, setIsFormValid]);
+
+  const handleRegister = () => {
+    console.log("hii");
+  };
+  return (
+    <AuthBox>
+      <Typography variant="h5" sx={{ color: "white" }}>
+        Create an account
+      </Typography>
+      <RegisterForm
+        mail={mail}
+        setMail={setMail}
+        password={password}
+        setPassword={setPassword}
+        username={username}
+        setUsername={setUsername}
+      />
+      <RegisterPageFooter
+        isFormValid={isFormValid}
+        handleLogin={handleRegister}
+      />
+    </AuthBox>
+  );
 };
 
 export default Register;
