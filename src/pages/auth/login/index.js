ae92d793878e1a9b295ea989c42904b6b0596966
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-28 15:30:57 +0630

    login page added

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AuthBox from "../../../shared/components/AuthBox";
+import { validateLoginForm } from "../../../shared/utils/validators";
+import LoginPageFooter from "./footer";
+import LoginHeader from "./header";
+import LoginPageForm from "./LoginPageForm";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password, setIsFormValid]);
+
+  const handleLogin = () => {
+    console.log("hii");
+  };
+  return (
+    <AuthBox>
+      <LoginHeader />
+      <LoginPageForm
+        mail={mail}
+        setMail={setMail}
+        password={password}
+        setPassword={setPassword}
+      />
+      <LoginPageFooter isFormValid={isFormValid} handleLogin={handleLogin} />
+    </AuthBox>
+  );
 };
 
 export default Login;
