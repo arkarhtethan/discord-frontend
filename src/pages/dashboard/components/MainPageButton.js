00a4b515a5d0254d0d7ff376568edb4aca0b9039
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-01 12:29:07 +0630

    main page button added

---
+++ src/pages/dashboard/components/MainPageButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { Group } from "@mui/icons-material";
+const MainPageButton = () => {
+  return (
+    <Button
+      style={{
+        width: "48px",
+        height: "48px",
+        borderRadius: "16px",
+        margin: 0,
+        padding: 0,
+        minWidth: "10px",
+        color: "white",
+        backgrondColor: "#5865F2",
+      }}
+    >
+      <Group />
+    </Button>
+  );
+};
+
+export default MainPageButton;
