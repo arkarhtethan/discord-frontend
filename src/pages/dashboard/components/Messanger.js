4941e89e7d57b01b0386bc5311ca3ec91a438023
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-01 12:22:07 +0630

    Messanger wrapper desgn added

---
+++ src/pages/dashboard/components/Messanger.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { styled } from "@mui/system";
+
+const Wrapper = styled("div")({
+  flexGrow: 1,
+  backgroundColor: "#36393f",
+  marginTop: "48px",
+  display: "flex",
+});
+
+const Messanger = () => {
+  return <Wrapper>Messanger</Wrapper>;
+};
+
+export default Messanger;
