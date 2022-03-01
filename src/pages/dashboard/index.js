c1a990913423c5c5d1a4c237f3f930cd207cc1d2
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-01 12:22:19 +0630

    dashboard compoent added

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { styled } from "@mui/system";
+import Sidebar from "./components/Sidebar";
+import FriendsSideBar from "./components/FriendsSideBar";
+import Messanger from "./components/Messanger";
+import AppBar from "./components/AppBar";
+
+const Wrapper = styled("div")({
+  width: "100%",
+  height: "100vh",
+  display: "flex",
+});
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Wrapper>
+      <Sidebar />
+      <FriendsSideBar />
+      <Messanger />
+      <AppBar />
+    </Wrapper>
+  );
 };
 
 export default Dashboard;
