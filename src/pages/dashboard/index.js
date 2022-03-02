7a7a997099b820f011258048339bca9bbb85752f
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-02 11:08:37 +0630

    load user from localstorage

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { styled } from "@mui/system";
 import Sidebar from "./components/Sidebar";
 import FriendsSideBar from "./components/FriendsSideBar";
 import Messanger from "./components/Messanger";
 import AppBar from "./components/AppBar";
+import { logout } from "../../shared/utils/auth";
+import { connect } from "react-redux";
+import { getActions } from "../../store/actions/authActions";
 
 const Wrapper = styled("div")({
   width: "100%",
@@ -11,7 +14,16 @@ const Wrapper = styled("div")({
   display: "flex",
 });
 
-const Dashboard = () => {
+const Dashboard = ({ setUserDetails }) => {
+  useEffect(() => {
+    const userDetails = localStorage.getItem("user");
+    if (!userDetails) {
+      logout();
+    } else {
+      setUserDetails(JSON.parse(userDetails));
+    }
+  }, []);
+
   return (
     <Wrapper>
       <Sidebar />
@@ -21,5 +33,9 @@ const Dashboard = () => {
     </Wrapper>
   );
 };
-
-export default Dashboard;
+const mapActionsToProps = (dispatch) => {
+  return {
+    ...getActions(dispatch),
+  };
+};
+export default connect(null, mapActionsToProps)(Dashboard);
