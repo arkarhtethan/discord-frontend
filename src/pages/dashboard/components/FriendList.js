193c4c9607d8efa5d1b03e6453a40268c612bc2f
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-02 11:12:12 +0630

    FriendList cmp added

---
+++ src/pages/dashboard/components/FriendList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { styled } from "@mui/system";
+import FriendsListItem from "./FriendsListItem";
+const MainContainer = styled("div")({
+  flexGrow: 1,
+  width: "100%",
+});
+
+const DUMMY_FRIENDS = [
+  { id: 1, username: "Mark", isOnline: true },
+  { id: 2, username: "Anna", isOnline: false },
+  { id: 2, username: "JOhn", isOnline: false },
+];
+
+const FriendList = () => {
+  return (
+    <MainContainer>
+      {DUMMY_FRIENDS.map((item) => (
+        <FriendsListItem key={item.id} {...item} />
+      ))}
+    </MainContainer>
+  );
+};
+
+export default FriendList;
